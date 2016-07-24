06876457fe53ca042d3349291bfe0fd5891a99af
abhishekraok <abhishkerao5002@gmail.com>
2016-07-23 17:51:18 -0700

    Fixed First LSTM model

--- src/FirstLSTMModel.py
+++ src/FirstLSTMModel.py
@@ -8,7 +8,7 @@ import numpy as np
 class FirstLSTMModel(object):
     def __init__(self):
         self.model = Sequential()
-        self.model.add(LSTM(128, input_shape=(5,Constants.MaxVocabulary)))
+        self.model.add(LSTM(128, input_shape=(1,Constants.MaxVocabulary)))
         self.model.add(Dense(Constants.MaxVocabulary))
         self.model.add(Activation('softmax'))
         optimizer = RMSprop(lr=0.01)
@@ -28,7 +28,7 @@ class FirstLSTMModel(object):
 
 if __name__ == '__main__':
     model = FirstLSTMModel()
-    x = np.random.random((100,5, Constants.MaxVocabulary))
+    x = np.random.random((100,1, Constants.MaxVocabulary))
     y = np.random.random((100,Constants.MaxVocabulary))
     model.train(x,y)
     print model.evaluate(x,y)
