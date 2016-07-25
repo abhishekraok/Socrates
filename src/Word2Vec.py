ab3c53bdd671a5b87aecf2de8695c1c62303b46e
Kandarp Makwana <kandarp2516@gmail.com>
2016-07-25 15:41:03 -0700

    added test stub in Word2Vec.py

--- src/Word2Vec.py
+++ src/Word2Vec.py
@@ -1,5 +1,5 @@
 import gensim
-class Word2Vec(Object):
+class Word2Vec(object):
 	#initializer
 	# path: path for vector binary dataset
 	def __init__(self,path='../data/GoogleNews-vectors-negative300.bin'):
@@ -12,5 +12,12 @@ class Word2Vec(Object):
 
 	#gets top_n words for particular vector
 	# vector: wordvector n: number of similar words required
-	def get_words(self,vector,n):
+	def get_words(self,vector,n=10):
 		return model.similar_by_vector(vector,topn=n)
+if __name__=="__main__":
+	model=Word2Vec()
+	word="congratulations"
+	vector=model.get_vector(word)
+	print "vector for ",word,":",vector
+	derived_word=model.get_words(vector,1)
+	print "derived word from the vector:",derived_word
