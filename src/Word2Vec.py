51e97526e266ceaf67530ba4112318d791c53f22
Kandarp Makwana <kandarp2516@gmail.com>
2016-07-25 17:40:14 -0700

    Added get_top_word() to Word2Vec.py

--- src/Word2Vec.py
+++ src/Word2Vec.py
@@ -3,21 +3,23 @@ class Word2Vec(object):
 	#initializer
 	# path: path for vector binary dataset
 	def __init__(self,path='../data/GoogleNews-vectors-negative300.bin'):
-		model = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
+		self.model = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
 
 	# gets vector for particular word
 	# word: the word for which vector is requested
 	def get_vector(self,word):
-		return model[word]
+		return self.model[word]
 
 	#gets top_n words for particular vector
 	# vector: wordvector n: number of similar words required
 	def get_words(self,vector,n=10):
-		return model.similar_by_vector(vector,topn=n)
+		return self.model.similar_by_vector(vector,topn=n)[0][0]
+	def get_top_word(self,vector):
+		return self.get_words(vector,1)
 if __name__=="__main__":
 	model=Word2Vec()
 	word="congratulations"
 	vector=model.get_vector(word)
-	print "vector for ",word,":",vector
-	derived_word=model.get_words(vector,1)
+	#print "vector for ",word,":",vector
+	derived_word=model.get_top_word(vector)
 	print "derived word from the vector:",derived_word
